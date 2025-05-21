eb7e559f342148fe2be135fb38a814d066ed2be1
Finney Student <Finney Student>
2025-05-20 20:25:54 -0400

    adding all of zac's changes from 5/20

---
+++ what_ever_you_think_makes_sense/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Car myCar = new Car();
+        System.out.println( myCar. getspeed());
+        myCar.getspeed(59.0);
+    }
+}
---
+++ what_ever_you_think_makes_sense/src/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+
+    private double speed = 0;
+    public void exelorate (double pressure) {
+        speed = pressure;
+    }
+
+    public double getspeed (){
+        return speed;
+    }
+    public double getspeed (){
+        
+    }
+
+
+}
