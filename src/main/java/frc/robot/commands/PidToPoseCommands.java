f37461787eb2591d33e98176a1fbd5a9ff8775ad
Stephen Cerbone <importsjc@gmail.com>
2025-06-02 11:25:47 -0400

    adding left side (from DS perspective) 3 coral auto named 'P2P_Blue_3A_2B_ParallelIntake' tested in sim

--- src/main/java/frc/robot/commands/PidToPoseCommands.java
+++ src/main/java/frc/robot/commands/PidToPoseCommands.java
@@ -13,20 +13,39 @@ import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class PidToPoseCommands {
+  /* Reef Poses */
   public static Pose2d Red_5A = new Pose2d(4.98, 2.84, Rotation2d.fromDegrees( -60));
   public static Pose2d Red_5B = new Pose2d(5.270, 3.000, Rotation2d.fromDegrees(-60));
   public static Pose2d Red_6A = new Pose2d(3.71, 3, Rotation2d.fromDegrees(-120.0));
   public static Pose2d Red_6B = new Pose2d(3.99, 2.84, Rotation2d.fromDegrees(-120.0));
 
+  public static Pose2d Blue_2A = new Pose2d(3.99, 5.21, Rotation2d.fromDegrees(120));
+  public static Pose2d Blue_2B = new Pose2d(3.71, 5.05, Rotation2d.fromDegrees( 120));
+  public static Pose2d Blue_3A = new Pose2d(5.27, 5.05, Rotation2d.fromDegrees(60));
+
+  /* Feeder Station Poses */
+  public static Pose2d LeftFeeder = new Pose2d(0.98, 7.05, Rotation2d.fromDegrees(125.0));
+  public static Pose2d RightFeeder = new Pose2d(0.98, 1, Rotation2d.fromDegrees(-125.0));
+
   public static void registerCommands(CommandSwerveDrivetrain drivetrain) {
+    /* Commands */
     Command MoveTo_Red_5B = drivetrain.runPidToPose(Red_5B, 1, true);
     Command MoveTo_Red_5A = drivetrain.runPidToPose(Red_5A, 1, true);
     Command MoveTo_Red_6B = drivetrain.runPidToPose(Red_6B, 1, true);
     Command MoveTo_Red_6A = drivetrain.runPidToPose(Red_6A, 1, true);
 
+    Command MoveTo_Blue_2A = drivetrain.runPidToPose(Blue_2A, 1, true);
+    Command MoveTo_Blue_2B = drivetrain.runPidToPose(Blue_2B, 1, true);
+    Command MoveTo_Blue_3A = drivetrain.runPidToPose(Blue_3A, 1, true);
+
+    /* Register Commands */
     NamedCommands.registerCommand("MoveTo_Red_5B", MoveTo_Red_5B);
     NamedCommands.registerCommand("MoveTo_Red_5A", MoveTo_Red_5A);
     NamedCommands.registerCommand("MoveTo_Red_6B", MoveTo_Red_6B);
     NamedCommands.registerCommand("MoveTo_Red_6A", MoveTo_Red_6A);
+
+    NamedCommands.registerCommand("MoveTo_Blue_2A", MoveTo_Blue_2A);
+    NamedCommands.registerCommand("MoveTo_Blue_2B", MoveTo_Blue_2B);
+    NamedCommands.registerCommand("MoveTo_Blue_3A", MoveTo_Blue_3A);
   }
 }
