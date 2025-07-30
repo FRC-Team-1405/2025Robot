9798a0813633326a6dc9325feae536aa29564942
Finney Student <Finney Student>
2025-07-29 20:16:31 -0400

    Changed HashMap to a TreeMap

--- CarProject/CarProject/src/GenreHandler.java
+++ CarProject/CarProject/src/GenreHandler.java
@@ -1,26 +1,26 @@
-import java.util.HashMap;
+import java.util.TreeMap;
 
 public class GenreHandler{
     public static String StationToGenre(String Station) {
         
         // a station object that we insert into a map
-        Station myStation = new Station();
+         Station myStation = new Station();
+
         myStation.frequency = 100.3;
 
         // create a new map to associate stations with genres
-        HashMap<Station,Genre> StationGenre = new HashMap<Station,Genre>();
+        TreeMap<Station,Genre> StationGenre = new TreeMap<Station,Genre>();
 
         // insert a station into the station/genre map
         StationGenre.put(myStation, Genre.COUNTRY);
+       
+        Station myStationTwo = new Station();
 
+        myStationTwo.frequency = Double.valueOf(Station);
 
-        // Station x = (Station)StationGenre.keySet().toArray()[0];
-        // take the input to this method, and create a new station to search through the map with
-        Station frequencyToStation = new Station();
-        frequencyToStation.frequency = Double.valueOf(Station);
-
+    
         // search through the map and retrieve the associated genre
-        Genre gottenGenre = StationGenre.get(frequencyToStation);
+        Genre gottenGenre = StationGenre.get(myStationTwo);
 
         // return the associated genre
         return gottenGenre.name();
