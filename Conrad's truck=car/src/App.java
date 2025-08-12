8ec230d8b8e1c8d75c5f5051920ae7f53ddde712
Finney Student <Finney Student>
2025-08-12 18:43:32 -0400

    the lost ark

--- Conrad's truck=car/src/App.java
+++ Conrad's truck=car/src/App.java
@@ -1,13 +1,40 @@
+import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("on your mark get set Goooo!!!!!!");
         Car myCar = new Car();
         Random random = new Random();
-        int randomInt = random.ints(-20, 1).findFirst().getAsInt();
+        int randomInt = random.ints(0, 100).findFirst().getAsInt();
         myCar.exelorate (randomInt);
-        System.out.println("Random number: " + randomInt);
-        System.out.println( myCar. getspeed());
+    
+        Car myCar2 = new Car();
+        Random random2 = new Random();
+        int randomInt2 = random2.ints(0, 100).findFirst().getAsInt();
+        myCar2.exelorate (randomInt2);
+
+       //HashMap<Car, int> CarSpeedSorter;
+
+
+        System.out.println("Car1 speed: " + randomInt);
+
+        System.out.println("Car2 speed: " + randomInt2);
+
+        if (myCar2.Crash()) { System.out.println("Car2 Crashed in an fiery inferno");
+        
+        }
+
+        if (myCar.Crash()) { System.out.println("Car1 Crashed in an fiery inferno"); 
+
+        }
+
+        if (myCar.getspeed() < myCar2.getspeed()) {System.out.println("Car2 Wins!!!!!");
+            
+        }
+        if (myCar.getspeed() > myCar2.getspeed()) {System.out.println("Car1 Wins!!!!!");
+            
+        }
     }
 }
--- Conrad's truck=car/src/Car.java
+++ Conrad's truck=car/src/Car.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Car {
 
     private double speed = 0;
@@ -8,6 +10,13 @@ public class Car {
     public double getspeed (){
         return speed;
     }
-
+public boolean Crash() {
+    Random randomcrash = new Random();
+        int randomIntcrash = randomcrash.ints(0,101).findFirst().getAsInt();
+        if (randomIntcrash < 13){
+        return true;
+        }
+        return false;
+}
 
 }
