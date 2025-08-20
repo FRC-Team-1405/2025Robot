9f2d744d8350c9e8f500f75d592a630be6196652
Finney Student <Finney Student>
2025-08-19 20:05:46 -0400

    i made the drive method *yay*

--- CarProject/CarProject/src/App.java
+++ CarProject/CarProject/src/App.java
@@ -4,17 +4,20 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println ("hi") ;
         System.out.println (2+1) ;
-        int x = 3*2 ;
+        int x = 30-3 ;
+        int z = 10 ;
         System.out.println (x);
-        if (x>10){
+        if (x<100){
             System.out.println ("yay it worked!");
         }
         else{
-            System.out.println ("I hope this works") ;  
+            System.out.println (" it worked again! ") ;  
         }
         Car newCar = new Car () ;
         System.out.println (newCar.gas) ;
         newCar.fillTank(x) ;
         System.out.println (newCar.gas) ;
+        newCar.emptyTank(z) ;
+        System.out.println (newCar.gas); //TODO: use drive method 
      }
 }
--- CarProject/CarProject/src/Car.java
+++ CarProject/CarProject/src/Car.java
@@ -1,8 +1,17 @@
 public class Car {
-int gas = 10 ;
-public int fillTank ( int gallonsToAdd ) {
- gas = gas - gallonsToAdd ; 
- return gas ;
-};
-
+    double mpg = 30.5 ;
+    double gas = 10 ;
+    public double fillTank ( double gallonsToAdd ) {
+        gas = gas + gallonsToAdd ; 
+        return gas ;
+    };
+    public double emptyTank ( double gallonsToLoose) {
+        gas = gas - gallonsToLoose ;
+        return gas ;
+    }
+    public double drive ( double milesToDrive ) {
+      double gallonsToLoose = milesToDrive / mpg ;
+        emptyTank (gallonsToLoose); 
+        return gas ;
+    }
 }
