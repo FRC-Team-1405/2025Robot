b829a53cc536c18dfdc5da075d3d63a87b01d67c
Andrew <none>
2025-09-09 20:13:12 -0400

    started hashmap

--- what_ever_you_think_makes_sense/src/App.java
+++ what_ever_you_think_makes_sense/src/App.java
@@ -1,25 +1,33 @@
+import java.util.HashMap;
 import java.util.Random;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Random random = new Random();
-        double randomDouble = random.nextDouble();  //random number generator
-        double randomDouble2 = random.nextDouble(); 
-        double randomDouble3 = random.nextDouble(); 
 
+    public static void main(String[] args) throws Exception {
+     
+     HashMap  <Car, double > 
+     .put myCar , randomint;
+     .put myCar2, randomint2;
+     .put myCar3, randomint3;
+     
+        Random random = new Random();
+        double randomint = random.nextDouble(100,150);   //random number generator
+        double randomint2 = random .nextDouble(100,150); 
+        double randomint3 = random. nextDouble(100,150);
+       // double randomint4 = random 
 
         Car myCar = new Car();                       //car 1 speed
-        myCar.exelorate(randomDouble);
+        myCar.exelorate(randomint);
         System.out.println( myCar. getspeed());
         
 
         Car myCar2 = new Car();                      //car 2 speed
-        myCar2.exelorate(randomDouble2);
+        myCar2.exelorate(randomint2);
         System.out.println(myCar2.getspeed()); 
 
 
         Car myCar3 = new Car();                      //car 3 speed
-        myCar3.exelorate(randomDouble3);
+        myCar3.exelorate(randomint3);
         System.out.println(myCar3.getspeed()); 
 
 
@@ -27,25 +35,34 @@ public class App {
 
 
         if (myCar.getspeed()> myCar2.getspeed()&& myCar2.getspeed()>myCar3.getspeed()){  
-            System.out.println("1 - 2 - 3");
+            System.out.println("FORD - HONDA - KIA");
         }
         if (myCar.getspeed()> myCar3.getspeed()&& myCar3.getspeed()>myCar2.getspeed()){    
-            System.out.println("1 - 3 - 2");
+            System.out.println("FORD - KIA - HONDA");
         }
         if (myCar2.getspeed()> myCar.getspeed()&& myCar.getspeed()>myCar3.getspeed()){    
-            System.out.println("2 - 1 - 3");
+            System.out.println("HONDA - FORD - KIA");
         }
         if (myCar2.getspeed()> myCar3.getspeed()&& myCar3.getspeed()>myCar.getspeed()){    
-            System.out.println("2 - 3 - 1");
+            System.out.println("HONDA - KIA - FORD");
         }
         if (myCar3.getspeed()> myCar2.getspeed()&& myCar2.getspeed()>myCar.getspeed()){   
-            System.out.println("3 - 2 - 1");
+            System.out.println("KIA - HONDA - FORD");
         }
         if (myCar3.getspeed()> myCar.getspeed()&& myCar.getspeed()>myCar2.getspeed()){    
-            System.out.println("3 - 1 - 2");
+            System.out.println("KIA - FORD - HONDA");
         }
-       
 
+
+        if ( myCar2.getspeed() > 145){    
+            System.out.println(" HONDA False Start");
+        }
+        if ( myCar3.getspeed() > 145){    
+            System.out.println(" KIA False Start");
+        }
+        if ( myCar.getspeed() > 145){    
+            System.out.println(" FORD False Start");
+        }
         }
 
     }
--- what_ever_you_think_makes_sense/src/Car.java
+++ what_ever_you_think_makes_sense/src/Car.java
@@ -9,5 +9,6 @@ public class Car {
         return speed;
     }
     
+    
 
 }
